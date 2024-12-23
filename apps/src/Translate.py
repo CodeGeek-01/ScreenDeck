9981494ad3f2d01415cf50d6f72895b1a0cc0d71
CodeGeek01 <156606490+CodeGeek-01@users.noreply.github.com>
2024-12-23 12:37:46 +0100

    Update Translate.py

--- apps/src/Translate.py
+++ apps/src/Translate.py
@@ -1,3 +1,4 @@
+from langdetect import detect
 import urllib.request
 import urllib.parse
 import pyperclip
@@ -5,33 +6,55 @@ import keyboard
 import json
 import time
 
-
 LANGUAGE_MAP = {
-  "af": "Afrikaans", "sq": "Albanian", "am": "Amharic", "ar": "Arabic", 
-  "hy": "Armenian", "az": "Azerbaijani", "eu": "Basque", "be": "Belarusian", 
-  "bn": "Bengali", "bs": "Bosnian", "bg": "Bulgarian", "ca": "Catalan", 
-  "ceb": "Cebuano", "zh-CN": "Chinese (Simplified)", "zh-TW": "Chinese (Traditional)", 
-  "hr": "Croatian", "cs": "Czech", "da": "Danish", "nl": "Dutch", "en": "English", 
-  "eo": "Esperanto", "et": "Estonian", "fi": "Finnish", "fr": "French", 
-  "gl": "Galician", "ka": "Georgian", "de": "German", "el": "Greek", 
-  "gu": "Gujarati", "ht": "Haitian Creole", "ha": "Hausa", "haw": "Hawaiian", 
-  "he": "Hebrew", "hi": "Hindi", "hmn": "Hmong", "hu": "Hungarian", "is": "Icelandic", 
-  "ig": "Igbo", "id": "Indonesian", "ga": "Irish", "it": "Italian", "ja": "Japanese", 
-  "jv": "Javanese", "kn": "Kannada", "kk": "Kazakh", "km": "Khmer", "rw": "Kinyarwanda", 
-  "ko": "Korean", "ku": "Kurdish", "ky": "Kyrgyz", "lo": "Lao", "la": "Latin", 
-  "lv": "Latvian", "lt": "Lithuanian", "lb": "Luxembourgish", "mk": "Macedonian", 
-  "mg": "Malagasy", "ms": "Malay", "ml": "Malayalam", "mt": "Maltese", 
-  "mi": "Maori", "mr": "Marathi", "mn": "Mongolian", "my": "Myanmar (Burmese)", 
-  "ne": "Nepali", "no": "Norwegian", "ny": "Nyanja (Chichewa)", "or": "Odia", 
-  "ps": "Pashto", "fa": "Persian", "pl": "Polish", "pt": "Portuguese", "pa": "Punjabi", 
-  "ro": "Romanian", "ru": "Russian", "sm": "Samoan", "gd": "Scots Gaelic", 
-  "sr": "Serbian", "st": "Sesotho", "sn": "Shona", "sd": "Sindhi", "si": "Sinhala", 
-  "sk": "Slovak", "sl": "Slovenian", "so": "Somali", "es": "Spanish", "su": "Sundanese", 
-  "sw": "Swahili", "sv": "Swedish", "tl": "Tagalog (Filipino)", "tg": "Tajik", 
-  "ta": "Tamil", "tt": "Tatar", "te": "Telugu", "th": "Thai", "tr": "Turkish", 
-  "tk": "Turkmen", "uk": "Ukrainian", "ur": "Urdu", "ug": "Uyghur", "uz": "Uzbek", 
-  "vi": "Vietnamese", "cy": "Welsh", "xh": "Xhosa", "yi": "Yiddish", "yo": "Yoruba", 
-  "zu": "Zulu"
+  "sq": "Albanian",
+  "ar": "Arabic",
+  "az": "Azerbaijani",
+  "eu": "Basque",
+  "bn": "Bengali",
+  "bg": "Bulgarian",
+  "ca": "Catalan",
+  "zh": "Chinese",
+  "zt": "Chinese (traditional)",
+  "cs": "Czech",
+  "da": "Danish",
+  "nl": "Dutch",
+  "en": "English",
+  "eo": "Esperanto",
+  "et": "Estonian",
+  "fi": "Finnish",
+  "fr": "French",
+  "gl": "Galician",
+  "de": "German",
+  "el": "Greek",
+  "he": "Hebrew",
+  "hi": "Hindi",
+  "hu": "Hungarian",
+  "id": "Indonesian",
+  "ga": "Irish",
+  "it": "Italian",
+  "ja": "Japanese",
+  "ko": "Korean",
+  "lv": "Latvian",
+  "lt": "Lithuanian",
+  "ms": "Malay",
+  "nb": "Norwegian",
+  "fa": "Persian",
+  "pl": "Polish",
+  "pt": "Portuguese",
+  "ro": "Romanian",
+  "ru": "Russian",
+  "sr": "Serbian",
+  "sk": "Slovak",
+  "sl": "Slovenian",
+  "es": "Spanish",
+  "sv": "Swedish",
+  "tl": "Tagalog",
+  "th": "Thai",
+  "tr": "Turkish",
+  "uk": "Ukrainian",
+  "ur": "Urdu",
+  "vi": "Vietnamese"
 }
 
 
@@ -53,30 +76,25 @@ def translate_translate():
 
 
 def translate(text, target_language, source_language="auto"):
-  base_url = "https://translate.googleapis.com/translate_a/single"
+  if source_language == "auto":
+    source_language = detect(text)
 
-  encoded_text = urllib.parse.quote(text)
-
+  url = "https://api.mymemory.translated.net/get"
+    
   params = {
-    "client": "gtx",
-    "sl": source_language,
-    "tl": target_language,
-    "dt": "t",
-    "q": encoded_text,
+    "q": text,
+    "langpair": f"{source_language}|{target_language}"
   }
-
-  url = f"{base_url}?{urllib.parse.urlencode(params)}"
-
-  with urllib.request.urlopen(url) as response:
-    result = json.loads(response.read())
-
-  translated_text = ""
-  for i in result[0]:
-    translated_text += i[0]
-  fromLang = result[2]
-
-  translated_text = urllib.parse.unquote(translated_text)
-  return [fromLang, translated_text]
+  
+  url_params = urllib.parse.urlencode(params)
+  request_url = f"{url}?{url_params}"
+  
+  try:
+    with urllib.request.urlopen(request_url) as response:
+      result = json.load(response)
+      return [source_language, result['responseData']['translatedText']]
+  except urllib.error.URLError as e:
+    return ["None", {e}]
 
 
 def get_selected_text():
@@ -207,115 +225,54 @@ def load():
     
     <div class="langSelect">
       <select id="language-select" name="language">
-        <option value="af">Afrikaans</option>
         <option value="sq">Albanian</option>
-        <option value="am">Amharic</option>
         <option value="ar">Arabic</option>
-        <option value="hy">Armenian</option>
         <option value="az">Azerbaijani</option>
         <option value="eu">Basque</option>
-        <option value="be">Belarusian</option>
         <option value="bn">Bengali</option>
-        <option value="bs">Bosnian</option>
         <option value="bg">Bulgarian</option>
         <option value="ca">Catalan</option>
-        <option value="ceb">Cebuano</option>
-        <option value="zh-CN">Chinese (Simplified)</option>
-        <option value="zh-TW">Chinese (Traditional)</option>
-        <option value="co">Corsican</option>
-        <option value="hr">Croatian</option>
+        <option value="zh">Chinese</option>
+        <option value="zt">Chinese (traditional)</option>
         <option value="cs">Czech</option>
         <option value="da">Danish</option>
         <option value="nl">Dutch</option>
         <option value="en">English</option>
         <option value="eo">Esperanto</option>
         <option value="et">Estonian</option>
-        <option value="tl">Filipino</option>
         <option value="fi">Finnish</option>
         <option value="fr">French</option>
-        <option value="fy">Frisian</option>
         <option value="gl">Galician</option>
-        <option value="ka">Georgian</option>
         <option value="de">German</option>
         <option value="el">Greek</option>
-        <option value="gu">Gujarati</option>
-        <option value="ht">Haitian Creole</option>
-        <option value="ha">Hausa</option>
-        <option value="haw">Hawaiian</option>
         <option value="he">Hebrew</option>
         <option value="hi">Hindi</option>
-        <option value="hmn">Hmong</option>
         <option value="hu">Hungarian</option>
-        <option value="is">Icelandic</option>
-        <option value="ig">Igbo</option>
         <option value="id">Indonesian</option>
         <option value="ga">Irish</option>
         <option value="it">Italian</option>
         <option value="ja">Japanese</option>
-        <option value="jv">Javanese</option>
-        <option value="kn">Kannada</option>
-        <option value="kk">Kazakh</option>
-        <option value="km">Khmer</option>
-        <option value="rw">Kinyarwanda</option>
         <option value="ko">Korean</option>
-        <option value="ku">Kurdish (Kurmanji)</option>
-        <option value="ky">Kyrgyz</option>
-        <option value="lo">Lao</option>
-        <option value="la">Latin</option>
         <option value="lv">Latvian</option>
         <option value="lt">Lithuanian</option>
-        <option value="lb">Luxembourgish</option>
-        <option value="mk">Macedonian</option>
-        <option value="mg">Malagasy</option>
         <option value="ms">Malay</option>
-        <option value="ml">Malayalam</option>
-        <option value="mt">Maltese</option>
-        <option value="mi">Maori</option>
-        <option value="mr">Marathi</option>
-        <option value="mn">Mongolian</option>
-        <option value="my">Myanmar (Burmese)</option>
-        <option value="ne">Nepali</option>
-        <option value="no">Norwegian</option>
-        <option value="ny">Nyanja (Chichewa)</option>
-        <option value="or">Odia (Oriya)</option>
-        <option value="ps">Pashto</option>
-        <option value="fa">Persian</option>
+        <option value="nb">Norwegian</option>
+        <option value="fa" selected="">Persian</option>
         <option value="pl">Polish</option>
         <option value="pt">Portuguese</option>
-        <option value="pa">Punjabi</option>
         <option value="ro">Romanian</option>
         <option value="ru">Russian</option>
-        <option value="sm">Samoan</option>
-        <option value="gd">Scots Gaelic</option>
         <option value="sr">Serbian</option>
-        <option value="st">Sesotho</option>
-        <option value="sn">Shona</option>
-        <option value="sd">Sindhi</option>
-        <option value="si">Sinhala (Sinhalese)</option>
         <option value="sk">Slovak</option>
         <option value="sl">Slovenian</option>
-        <option value="so">Somali</option>
         <option value="es">Spanish</option>
-        <option value="su">Sundanese</option>
-        <option value="sw">Swahili</option>
         <option value="sv">Swedish</option>
-        <option value="tg">Tajik</option>
-        <option value="ta">Tamil</option>
-        <option value="tt">Tatar</option>
-        <option value="te">Telugu</option>
+        <option value="tl">Tagalog</option>
         <option value="th">Thai</option>
         <option value="tr">Turkish</option>
-        <option value="tk">Turkmen</option>
         <option value="uk">Ukrainian</option>
         <option value="ur">Urdu</option>
-        <option value="ug">Uyghur</option>
-        <option value="uz">Uzbek</option>
         <option value="vi">Vietnamese</option>
-        <option value="cy">Welsh</option>
-        <option value="xh">Xhosa</option>
-        <option value="yi">Yiddish</option>
-        <option value="yo">Yoruba</option>
-        <option value="zu">Zulu</option>
       </select>
     </div>
 
